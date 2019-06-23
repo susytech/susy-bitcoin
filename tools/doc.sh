b450fa9f4e910180dbe8ab17e926913d6b54d65c
SUSYLABS <admin@superstring.ch>
2019-06-23 18:26:17 +0800

    initial update

---
+++ tools/doc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cargo doc --no-deps\
+	-p bitcrypto\
+	-p chain\
+	-p db\
+	-p import\
+	-p keys\
+	-p message\
+	-p miner\
+	-p network\
+	-p susybtc\
+	-p p2p\
+	-p primitives\
+	-p rpc\
+	-p script\
+	-p serialization\
+	-p sync\
+	-p test-data\
+	-p verification
